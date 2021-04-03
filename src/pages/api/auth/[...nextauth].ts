ba93b68ab0c294577a9bd40b9f146e5d38c5aecd
Joao Augusto Oliveira dos Santos <joaoaugustofurg@gmail.com>
2021-04-03 17:46:29 -0300

    :card_file_box: add faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,5 +12,38 @@ export default NextAuth({
       scope: 'read:user' // autorizações necessárias, nesse caso, a mais básica
     }),
   ],
+   callbacks: { // callback oriundo da doc do fauna
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+         q.If( // se nao existe um usuário com o respectivo e-mail
+           q.Not(
+             q.Exists(
+               q.Match(
+                 q.Index('user_by_email'), // index criado no fauna
+                 q.Casefold(user.email) // para não distinguir maiúsculas e minúsculas
+               )
+             )
+           ),
+           q.Create( // cria o usuário com esse e-mail na collection users
+             q.Collection('users'),
+             { data: { email}}
+           ),
+           q.Get( // se ele existe, busque pelo e-mail
+             q.Match(
+               q.Index('user_by_email'),
+               q.Casefold(user.email)
+             )
+           )
+         )
+      )
+        return true
+      } catch (error) {
+        return false
+      }     
+    },
+  }
 })
 
